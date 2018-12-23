07b537368d632d05272d27fd3d40eea51fa6daf3
vanluwin <richardsonsantiago@ufrn.edu.br>
2018-12-23 16:22:18 -0300

    gpu script

---
+++ install-gpu-driver.sh
@@ -0,0 +1,14 @@
+echo "Deleting other version of nvidia drivers..."
+sudo apt-get purge nvidia* 
+
+echo "Getting PPA..."
+sudo add-apt-repository ppa:graphics-drivers
+
+echo "Updating..."
+sudo apt-get update && sudo apt-get upgrade
+
+echo "Getting the driver"
+sudo apt-get install nvidia-390
+
+echo "Freezing driver version..."
+sudo apt-mark hold nvidia-390
