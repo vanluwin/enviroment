8de8baa0b999c5a847157cc375667078aa47da54
Richardson Santiago <richardsonsantiago@ufrn.edu.br>
2019-02-23 16:48:26 -0300

    Update install-opencv.sh

--- install-opencv.sh
+++ install-opencv.sh
@@ -8,7 +8,7 @@ sudo apt-get -y autoremove
 # 2. INSTALL THE DEPENDENCIES
 echo 'Installing the dependencies ...'
 
-sudo apt-get install -y build-essential cmake build-essential cmake git unzip pk    g-config libjpeg-dev libtiff5-dev libavcodec-dev libavformat-dev libswscale-dev     libv4l-dev libxvidcore-dev libx264-dev libgtk-3-dev libhdf5-serial-dev graphviz     libopenblas-dev libatlas-base-dev gfortran python-tk python3-tk python-imaging-t    k python2.7-dev python3-dev linux-image-generic linux-image-extra-virtual linux-    source linux-headers-generic unzip wget qt5-default
+sudo apt-get install -y build-essential cmake git unzip pkg-config libjpeg-dev libtiff5-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libgtk-3-dev libhdf5-serial-dev graphviz libopenblas-dev libatlas-base-dev gfortran python-tk python3-tk python-imaging-tk python2.7-dev python3-dev linux-image-generic linux-image-extra-virtual linux-source linux-headers-generic unzip wget qt5-default
 
 # 4. INSTALL THE LIBRARY (YOU CAN CHANGE '3.3.0' FOR THE LAST STABLE VERSION)
 echo 'Installing the library ...'
@@ -40,7 +40,7 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE \
     -D CUDA_FAST_MATH=1 \
     -D WITH_CUBLAS=1 \
     -D WITH_QT=1 \
-    -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-3.3.0/modules/ ..
+    -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-3.3.0/modules/
 
 make -j8
 sudo make install
