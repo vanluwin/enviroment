3f130a9dc0c7f22d2b22727620038109348b042f
vanluwin <richardsonsantiago@ufrn.edu.br>
2019-04-07 10:34:58 -0300

    Minor changes

--- install-cuda.sh
+++ install-cuda.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-wget https://developer.nvidia.com/compute/cuda/9.1/Prod/local_installers/cuda_9.1.85_387.26_linux.run
+wget https://developer.nvidia.com/compute/cuda/9.1/Prod/local_installers/cuda_9.1.85_387.26_linux -O cudaInstaler.run
 # if the previous fails download manually from https://developer.nvidia.com/compute/cuda/9.1/Prod/local_installers/cuda_9.1.85_387.26_linux
 
-chmod +x cuda_9.1.85_387.26_linux.run 
-./cuda_9.1.85_387.26_linux.run --extract=$HOME
+chmod +x cudaInstaler.run 
+./cudaInstaler.run --extract=$HOME
 
-cd ~
-sudo ./cuda-linux.9.1.85-23083092.run 
+cd $HOME
+sudo ./cudaInstaler.run 
 sudo bash -c "echo /usr/local/cuda/lib64/ > /etc/ld.so.conf.d/cuda.conf"
 sudo ldconfig
+
+echo "Done!"
